88e4722ce96d4425ca31ba309b78c8592cc8a947
Rogério De Oliveira <rogeriofilho632@gmail.com>
2020-01-14 17:37:14 -0300

    feat: PointSchema to location in Dev

--- backend/src/app/models/Dev.js
+++ backend/src/app/models/Dev.js
@@ -1,4 +1,12 @@
 import mongoose from 'mongoose';
+import PointSchema from '../../utils/PointSchema';
+
+/**
+ * O DevSchema foi criado como uma forma
+ * de armazenar os dev's cadastrados no sistema
+ * sem o compromisso de realizar relacionamentos com outras
+ * tabelas, sendo autossuficiente para o funcionamento do sistema.
+ */
 
 const DevSchema = new mongoose.Schema({
   name: String,
@@ -6,6 +14,16 @@ const DevSchema = new mongoose.Schema({
   bio: String,
   avatar_url: String,
   techs: [String],
+  /** o location foi passado aqui
+   *  para que pudessemos receber, junto
+   *  com as config antes realizadas,
+   *  para todas as info do user serem salvas
+   *  unicamente.
+   */
+  location: {
+    type: PointSchema,
+    index: '2dsphere'
+  },
 });
 
 export default mongoose.model('Dev', DevSchema);
