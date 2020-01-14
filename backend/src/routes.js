2f7f1fccb2bf124a8c8e74751e2e0df6300fb8a3
Rogério De Oliveira <rogeriofilho632@gmail.com>
2020-01-14 17:34:36 -0300

    refactor eslint 'n add routes to controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,37 +1,15 @@
 import { Router } from 'express';
-import axios from 'axios';
-import Dev from './app/models/Dev';
+
+import DevController from './app/controllers/DevController';
+import SearchController from './app/controllers/SearchController';
+
 
 const routes = new Router();
 
-/**
- * Query params
- * Route params
- * body params
- */
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-  routes.post('/devs', async (req, res) => {
-    const { github_username, techs } = req.body;
-
-    const response = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const { name = login, bio, avatar_url } = response.data;
-
-
-    // trim() elimina espaçamentos antes e depois do elemento
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-   const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray
-    });
-
-    return res.json(dev);
-
-  });
-
+routes.get('/search', SearchController.index);
 
 export default routes;
+
