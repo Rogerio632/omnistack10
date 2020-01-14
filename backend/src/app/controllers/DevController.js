431a680b3121718c05d3a9498753a0f46d1eb6b5
Rogério De Oliveira <rogeriofilho632@gmail.com>
2020-01-14 17:35:05 -0300

    feat: Dev 'n Search Controllers

---
+++ backend/src/app/controllers/DevController.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import Dev from '../models/Dev';
+import StringToArray from '../../utils/StringToArray';
+
+
+class DevController{
+
+async index(req, res){
+
+const devs = await Dev.find();
+
+return res.json(devs);
+
+}
+
+async store(req, res){
+
+  const { github_username, techs, latitude, longitude } = req.body;
+
+  const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  const { login, name = login, bio, avatar_url } = response.data;
+
+  let dev = await Dev.findOne({ github_username });
+
+  if(dev){
+    return res.json({error: "You are already registered in this website"});
+  }
+
+  // trim() elimina espaçamentos antes e depois do elemento
+    const techsArray = StringToArray(techs);
+
+  /*
+  ** Transforma a lont e lat recebida em uma localização pontual
+  ** o type e coordinates aqui preenchidas, são iniciadas no PointSchema.
+  */
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
+    dev = await Dev.create({
+     github_username,
+     name,
+     avatar_url,
+     bio,
+     techs: techsArray,
+     location
+   });
+
+  return res.json(dev);
+
+
+  }
+}
+
+export default new DevController();
---
+++ backend/src/app/controllers/SearchController.js
@@ -0,0 +1,38 @@
+import Dev from "../models/Dev";
+import StringToArray from '../../utils/StringToArray';
+
+class SearchController {
+
+async index (req, res){
+  // Buscar dev's num raio de 10km
+  // Filtro por tech
+
+  const { latitude, longitude, techs } = req.query;
+
+  const techsArray = StringToArray(techs);
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
+  const devs = await Dev.find({
+    techs: {
+      $in: techsArray,
+    },
+    location: {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates: [longitude, latitude]
+        },
+        $maxDistance: 10000,
+      }
+    },
+  });
+
+  return res.json({ devs });
+
+  }
+}
+export default new SearchController();
