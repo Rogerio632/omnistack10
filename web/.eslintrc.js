09be9b3daa8961bca9adc1500d9f519b108beda4
Rogério De Oliveira <rogeriofilho632@gmail.com>
2020-01-16 17:55:22 -0300

    default code text

---
+++ web/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+      "class-methods-use-this": "off",
+  "no-param-reassign": "off",
+  "camelcase": "off",
+  "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
+    }
+};
