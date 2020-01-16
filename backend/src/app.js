6d0146a9b6f5bd412381bcc49bf509db75380707
Rogério De Oliveira <rogeriofilho632@gmail.com>
2020-01-16 17:54:27 -0300

    add cors in server.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import routes from './routes';
 
 
@@ -14,6 +15,7 @@ class App {
 
   middlewares(){
     this.server.use(express.json());
+    this.server.use(cors());
   }
 
   routes(){
